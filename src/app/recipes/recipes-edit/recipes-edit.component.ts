3bfc7a25425d206e0353917f569df0191222f8df
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-07-02 22:15:06 +0200

    form-cd

--- src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipesService } from '../recipes.service';
 
 @Component({
@@ -46,8 +46,11 @@ export class RecipesEditComponent implements OnInit {
           for (let ingredient of recipe.ingredients) {
             recipeIngredients.push(
               new FormGroup({
-                'name': new FormControl(ingredient.name),
-                'amount': new FormControl(ingredient.amount),
+                'name': new FormControl(ingredient.name, Validators.required),
+                'amount': new FormControl(ingredient.amount, [
+                  Validators.required,
+                  Validators.pattern(/^[1-9]+[0-9]*$/)
+                ]),
               })
             );
           }
@@ -55,15 +58,27 @@ export class RecipesEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'recipeImagePath': new FormControl(recipeImagePath),
-      'recipeDescription': new FormControl(recipeDescription),
+      'name': new FormControl(recipeName, Validators.required),
+      'recipeImagePath': new FormControl(recipeImagePath, Validators.required),
+      'recipeDescription': new FormControl(recipeDescription, Validators.required),
       'ingredients': recipeIngredients
     });
   }
 
   getControls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ]),
+      })
+    );
   }
 
 }
